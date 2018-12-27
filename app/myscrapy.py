f233e10875da6b382974c427571cb0437d5e7574
Guilherme Tavares <zetavares.rib@gmail.com>
2018-12-27 21:44:38 -0200

    crawler

--- app/myscrapy.py
+++ app/myscrapy.py
@@ -1,1 +1,19 @@
 print('*' * 20)
+import asyncio
+
+from soup import Crawler
+
+url_ = 'https://www.epocacosmeticos.com.br/'
+
+crawler = Crawler()
+links = crawler.get_links(url_)
+
+for l in links:
+    print(l)
+
+url_ = 'https://www.epocacosmeticos.com.br/212-nyc-men-vintage-body-spray-carolina-herrera-perfume-masculino-para-o-corpo/p'
+crawler = Crawler()
+data_ = crawler.get_data(url_)
+
+
+print('*' * 20)
