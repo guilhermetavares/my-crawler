378283b49735bb130445a2927ba1cc5a2c39e95a
Guilherme Tavares <zetavares.rib@gmail.com>
2018-12-28 10:29:39 -0200

    recursive scrapy

--- app/myscrapy.py
+++ app/myscrapy.py
@@ -3,17 +3,36 @@ import asyncio
 
 from soup import Crawler
 
-url_ = 'https://www.epocacosmeticos.com.br/'
+url_ = 'https://www.epocacosmeticos.com.br/cabelos#5'
+
 
 crawler = Crawler()
 links = crawler.get_links(url_)
+ALREADY_URL = [
+    url_,
+]
 
 for l in links:
     print(l)
 
-url_ = 'https://www.epocacosmeticos.com.br/212-nyc-men-vintage-body-spray-carolina-herrera-perfume-masculino-para-o-corpo/p'
-crawler = Crawler()
-data_ = crawler.get_data(url_)
-
+# for href in links:
+#
+#     if href in ALREADY_URL:
+#         continue
+#
+#     ALREADY_URL.append(href)
+#
+#     for i in crawler.get_links(href):
+#         print(i)
+#
+#
+#
+# for l in links:
+#     print(l)
+#
+# url_ = 'https://www.epocacosmeticos.com.br/212-nyc-men-vintage-body-spray-carolina-herrera-perfume-masculino-para-o-corpo/p'
+# crawler = Crawler()
+# data_ = crawler.get_data(url_)
+#
 
 print('*' * 20)
--- app/soup.py
+++ app/soup.py
@@ -1,11 +1,15 @@
 import requests
 import re
 
+# from requests.exceptions import HTTPSConnectionPool
+
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 
 class Crawler(object):
+    VISITED_URLS = list()
+    DETAIL_URLS = list()
     REGEX_URL = re.compile(r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$')
 
     def get_soup(self, url):
@@ -13,28 +17,55 @@ class Crawler(object):
         soup = BeautifulSoup(contents, 'html.parser')
         return soup
 
+    def assert_product_link(self, link, regex='.*?/p$'):
+        return len(re.compile(regex).findall(link)) > 0
+
     def format_link(self, link, parse_url):
         href = link.get('href')
 
         if self.REGEX_URL.match(href):
             if re.search(parse_url.netloc, href):
                 return href
-
+            return None
         # abs
         return parse_url.scheme + '://' + parse_url.netloc + href
 
+
     def get_links(self, url):
+
+        # if self.REGEX_URL.match(url):
+        #     return []
+        #
+        if url in self.VISITED_URLS:
+            return []
+
+        self.VISITED_URLS.append(url)
+        print('get_links', url)
+
         soup = self.get_soup(url)
+        if soup is None:
+            return []
+
         parse_url = urlparse(url)
 
         links = list()
-
         for link in soup.findAll('a', attrs={'href': re.compile("/")}):
             _formated_link = self.format_link(link, parse_url)
+
+
+            if _formated_link and _formated_link in self.VISITED_URLS:
+                continue
+
             if _formated_link:
-                links.append(_formated_link)
+
+                if self.assert_product_link(_formated_link):
+                    links.append(_formated_link)
+                    continue
+
+                links += self.get_links(_formated_link)
 
         return links
+
 
     def get_data(self, url):
 
